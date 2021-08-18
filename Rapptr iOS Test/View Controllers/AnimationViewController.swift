feebacdcc9cd567b9ee6698539e85e429d79c123
Alex Paul <lexypaul13@yahoo.com>
2021-08-18 13:04:43 -0400

    New Features: Created custom loading animation

--- Rapptr iOS Test/View Controllers/AnimationViewController.swift
+++ Rapptr iOS Test/View Controllers/AnimationViewController.swift
@@ -7,28 +7,47 @@
 import UIKit
 
 class AnimationViewController: UIViewController {
-    
-    
+    /**
+     * =========================================================================================
+     * INSTRUCTIONS
+     * =========================================================================================
+     * 1) Make the UI look like it does in the mock-up.
+     *
+     * 2) Logo should fade out or fade in when the user hits the Fade In or Fade Out button
+     *
+     * 3) User should be able to drag the logo around the screen with his/her fingers
+     *
+     * 4) Add a bonus to make yourself stick out. Music, color, fireworks, explosions!!! Have Swift experience? Why not write the Animation
+     *    section in Swfit to show off your skills. Anything your heart desires!
+     *
+     */
+    //MARK:- Variables
     var panGesture =  UIPanGestureRecognizer()
-  
+    var tapGesture = UITapGestureRecognizer()
+    var isFadeIn = false
+    
+    //MARK:- Outlets
     @IBOutlet weak var imageLogoView: UIImageView!
     
-   var isFadeIn = false
+  
     
     
-    // MARK: - Lifecycle
+    // MARK: -  Viewiewntroller Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Animation"
+        
         panGesture = UIPanGestureRecognizer(target: self, action: #selector(draggedView(_:)))
         imageLogoView.isUserInteractionEnabled = true
         imageLogoView.addGestureRecognizer(panGesture)
         imageLogoView.fadeOut()
-
+        
+        tapGesture = UITapGestureRecognizer(target: self, action: #selector(onTapView))
+        self.view.addGestureRecognizer(tapGesture)
         
     }
     
-    // MARK: - Actions
+    // MARK: - IBAction
     @IBAction func backAction(_ sender: Any) {
         let mainMenuViewController = MenuViewController()
         self.navigationController?.pushViewController(mainMenuViewController, animated: true)
@@ -46,10 +65,25 @@ class AnimationViewController: UIViewController {
         
     }
     
-    @objc func draggedView(_ sender:UIPanGestureRecognizer){
-            self.view.bringSubviewToFront(imageLogoView)
-            let translation = sender.translation(in: self.view)
+    // MARK:- Objc Functions
+    @objc func draggedView(_ sender:UIPanGestureRecognizer){ //Performs fade-in animation
+        self.view.bringSubviewToFront(imageLogoView)
+        let translation = sender.translation(in: self.view)
         imageLogoView.center = CGPoint(x: imageLogoView.center.x + translation.x, y: imageLogoView.center.y + translation.y)
-            sender.setTranslation(CGPoint.zero, in: self.view)
+        sender.setTranslation(CGPoint.zero, in: self.view)
+    }
+    
+    @objc func onTapView() { //Performs fly animationa
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+            self.imageLogoView.transform = CGAffineTransform(translationX: -25, y: 0)
+        }) { (_) in
+            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+                self.imageLogoView.alpha = 0
+                self.imageLogoView.transform = self.imageLogoView.transform.translatedBy(x: 0, y: -200)
+            }, completion: nil)
         }
+        
+    }
+    
+    
 }
